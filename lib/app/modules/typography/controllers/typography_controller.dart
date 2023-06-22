5c668050e8ae2c82220161a00969bb2a4f0d7f40
Alpondith <sujonalimran@gmail.com>
2023-06-23 01:05:38 +0600

    typography

--- lib/app/modules/typography/controllers/typography_controller.dart
+++ lib/app/modules/typography/controllers/typography_controller.dart
@@ -1,15 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../core/util/font_size_manager.dart';
+import '../../../core/util/text_color_manager.dart';
 
 class TypographyController extends GetxController {
-  String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
-  List<double> sizes = [
-    FontSizeManager.TINY,
-    FontSizeManager.SMALL,
-    FontSizeManager.REGULAR,
-    FontSizeManager.LARGE,
-    FontSizeManager.XLARGE,
-    FontSizeManager.XXLARGE,
+  String text = "Lorem Ipsum is simply dummy text.";
+
+  List<Color> colors = [
+    TextColorManager.PRIMARY,
+    TextColorManager.PRIMARY_REGULAR,
+    TextColorManager.PRIMARY_LIGHT,
+    TextColorManager.BRAND_ACCENT,
+    TextColorManager.BRAND_ALPHA,
+    TextColorManager.BRAND_BETA,
+    TextColorManager.BRAND_GAMMA,
+    TextColorManager.BRAND_DELTA,
   ];
 }
--- lib/app/modules/typography/views/typography_view.dart
+++ lib/app/modules/typography/views/typography_view.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../widgets/app_container.dart';
+import '../../../widgets/atoms/texts.dart';
 import '../controllers/typography_controller.dart';
+import 'text_builder.dart';
 
 class TypographyView extends GetView<TypographyController> {
   const TypographyView({Key? key}) : super(key: key);
@@ -10,13 +13,73 @@ class TypographyView extends GetView<TypographyController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('TypographyView'),
-        centerTitle: true,
+        title: const Text('Typography'),
       ),
-      body: const Center(
-        child: Text(
-          'TypographyView is working',
-          style: TextStyle(fontSize: 20),
+      body: const SingleChildScrollView(
+        child: AppContainer(
+          child: SizedBox(
+            width: double.maxFinite,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // do not remove this comment line
+                TinyText('Tiny'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.TinyText),
+                SizedBox(height: 32),
+
+                SmallText('Small'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.SmallText),
+                SizedBox(height: 32),
+
+                SmallBoldText('Small Bold'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.SmallBoldText),
+                SizedBox(height: 32),
+
+                RegularText('Regular'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.RegularText),
+                SizedBox(height: 32),
+
+                RegularBoldText('Regular Bold'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.RegularBoldText),
+                SizedBox(height: 32),
+
+                LargeText('Large'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.LargeText),
+                SizedBox(height: 32),
+
+                LargeBoldText('Large Bold'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.LargeBoldText),
+                SizedBox(height: 32),
+
+                XLargeText('XLarge'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.XLargeText),
+                SizedBox(height: 32),
+
+                XLargeBoldText('XLarge Bold'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.XLargeBoldText),
+                SizedBox(height: 32),
+
+                XXLargeText('XXLarge'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.XXLargeText),
+                SizedBox(height: 32),
+
+                XXLargeBoldText('XXLarge Bold'),
+                SizedBox(height: 8),
+                TextBuilder(textType: TextType.XXLargeBoldText),
+                SizedBox(height: 32),
+              ],
+            ),
+          ),
         ),
       ),
     );
