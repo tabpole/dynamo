201fb76939b7eb9ce21c6ce4eca204d171ba92b2
Alpondith <sujonalimran@gmail.com>
2023-07-01 21:45:36 +0600

    asset folder structure changed

--- lib/app/core/util/asset_manager.dart
+++ lib/app/core/util/asset_manager.dart
@@ -1,20 +1,22 @@
 // ignore_for_file: constant_identifier_names
 
 class AssetManager {
+  // root path
+  static const String root = "lib/assets";
   // logo
-  static const String MAIN_LOGO = 'lib/assets/graphics/png/logo.png';
+  static const String MAIN_LOGO = '$root/png/logo.png';
 
   // jpg
-  static const String jpgPath = 'lib/assets/graphics/jpg';
+  static const String jpgPath = '$root/jpg';
   static const String AVATAR_JPG = '$jpgPath/avatar.jpg';
 
   // png
-  static const String pngPath = 'lib/assets/graphics/png';
-  static const String LOGO_PNG = '$pngPath/avatar.jpg';
+  static const String pngPath = '$root/png';
+  static const String LOGO_PNG = '$pngPath/logo.png';
 
   // svg
-  static const String svgPath = 'lib/assets/graphics/svg';
-  static const String USER_SVG = '$svgPath/avatar.jpg';
+  static const String svgPath = '$root/svg';
+  static const String USER_SVG = '$svgPath/user.svg';
 
   // lottie
   // gif
